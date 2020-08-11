f4b8d6a784d1c4ddc317dd1bc191f7c93365ff78
skyfrk <simon@esslinger.dev>
2020-08-11 23:52:08 +0000

    test: add first tests

--- src/FormulaInterpreter.Logic.Csharp.Test/CalculatorTest.cs
+++ src/FormulaInterpreter.Logic.Csharp.Test/CalculatorTest.cs
@@ -1,13 +1,51 @@
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace FormulaInterpreter.Logic.Csharp.Test
 {
     [TestClass]
     public class CalculatorTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow("1 + 1 = ", 2)]
+        [DataRow("1 - 1 = ", 0)]
+        [DataRow("1 - 1 + 1 = ", 1)]
+        [DataRow("1 + 1 - 1 = ", 1)]
+        [DataRow("1+1-1=", 1)]
+        [DataRow("7+8-1  =", 14)]
+        [DataRow("   7   +    8  - 1  =", 14)]
+        [DataRow("1-1-1-1", -4)]
+        [DataRow("1+1+1+1", 4)]
+        [DataRow("1+2", 3)]
+        [DataRow("1-3 ", -2)]
+        [DataRow(" 1+2", 3)]
+        public void Solve_ValidFormula_CorrectResult(string formula, int result)
         {
+            Calculator.Solve(formula).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataRow("   ")]
+        [DataRow("=")]
+        [DataRow(" = ")]
+        [DataRow("a")]
+        [DataRow("fail")]
+        [DataRow("*.:")]
+        [DataRow("1++1=")]
+        [DataRow("1+=")]
+        [DataRow("1+2-=")]
+        [DataRow("+2")]
+        [DataRow("2+2f")]
+        [DataRow("= 2 + 2 =")]
+        [DataRow("1+2++4=")]
+        public void Solve_InvalidFormula_ThrowsException(string formula, int result)
+        {
+            Action action = () => Calculator.Solve(formula);
+
+            action.Should().Throw<InvalidOperationException>();
         }
     }
 }
---
+++ src/FormulaInterpreter.Logic.Csharp.Test/LexerTest.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using FormulaInterpreter.Logic.Csharp.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormulaInterpreter.Logic.Csharp.Test
+{
+    [TestClass]
+    public class LexerTest
+    {
+        [TestMethod]
+        public void Tokenize_ValidFormula_CorrectTokens()
+        {
+            var tests = new Dictionary<string, Token[]>
+            {
+                {
+                    "1+1", new Token[] { new Token(TokenType.Number, 1), new Token(TokenType.Plus), new Token(TokenType.Number, 1) }
+                },
+                {
+                    "    1 -   2  ", new Token[] { new Token(TokenType.Number, 1), new Token(TokenType.Minus), new Token(TokenType.Number, 2) }
+                },
+                {
+                    "1-2", new Token[] { new Token(TokenType.Number, 1), new Token(TokenType.Minus), new Token(TokenType.Number, 2) }
+                },
+                {
+                    " 1 + 2 - 3 =", new Token[] { new Token(TokenType.Number, 1), new Token(TokenType.Plus), new Token(TokenType.Number, 2), new Token(TokenType.Minus), new Token(TokenType.Number, 3) }
+                },
+                {
+                    "1-2=", new Token[] { new Token(TokenType.Number, 1), new Token(TokenType.Minus), new Token(TokenType.Number, 2) }
+                },
+                {
+                    "1-+2=", new Token[] { new Token(TokenType.Number, 1), new Token(TokenType.Minus), new Token(TokenType.Plus), new Token(TokenType.Number, 2) }
+                },
+                {
+                    "123", new Token[] { new Token(TokenType.Number, 1), new Token(TokenType.Number, 2), new Token(TokenType.Plus), new Token(TokenType.Number, 3) }
+                },
+                {
+                    "1 2   3 ", new Token[] { new Token(TokenType.Number, 1), new Token(TokenType.Number, 2), new Token(TokenType.Plus), new Token(TokenType.Number, 3) }
+                }
+
+            };
+
+            foreach (var test in tests)
+            {
+                Lexer.Tokenize(test.Key).Should().BeEquivalentTo(test.Value);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow("a")]
+        [DataRow("fail")]
+        [DataRow("*.:")]
+        [DataRow("2+2f")]
+        [DataRow("a2 + 2 = 1")]
+        public void Tokenize_InvalidFormula_ThrowsException(string formula)
+        {
+            Action action = () => Lexer.Tokenize(formula);
+            action
+                .Should()
+                .Throw<InvalidOperationException>()
+                .WithMessage("Invalid char in formula.");
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataRow("   ")]
+        [DataRow("=")]
+        [DataRow(" =")]
+        [DataRow(" = ")]
+        public void Tokenize_EmptyFormula_ReturnsEmptyArray(string formula)
+        {
+            Lexer.Tokenize(formula).Should().BeEmpty();
+        }
+    }
+}
