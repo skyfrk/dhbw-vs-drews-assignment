f2a93571e417f9537adc1dfb976b209319418e51
skyfrk <simon@esslinger.dev>
2020-08-13 12:52:03 +0000

    style: remove whitespace

--- src/FormulaInterpreter.Logic.VisualBasic/Calculator.vb
+++ src/FormulaInterpreter.Logic.VisualBasic/Calculator.vb
@@ -5,25 +5,20 @@ Public Module Calculator
         If f.Length < 3 Then Throw New InvalidOperationException()
         Dim sum As Integer = 0
         Dim op As Char = "+"c
-
         For i As Integer = 0 To f.Length - 1
-
             If (f(i) = "+"c OrElse f(i) = "-"c) AndAlso op = "n"c Then
                 op = f(i)
                 Continue For
             End If
-
             If Char.IsDigit(f(i)) AndAlso op <> "n"c Then
                 If op = "+"c Then sum += CInt(Char.GetNumericValue(f(i)))
                 If op = "-"c Then sum -= CInt(Char.GetNumericValue(f(i)))
                 op = "n"c
                 Continue For
             End If
-
             If f(i) = " "c OrElse (f(i) = "="c AndAlso i + 1 = f.Length) Then Continue For
             Throw New InvalidOperationException()
         Next
-
         If op <> "n"c Then Throw New InvalidOperationException()
         Return sum
     End Function
